5ab5d52d9642e7d9134a42e075a99f04b1ae0d37
Nightmare8 <juan.caniguante@sansano.usm.cl>
2022-07-14 11:21:48 -0400

    add Button in medicamentos

--- src/routes/inventario.js
+++ src/routes/inventario.js
@@ -1,10 +1,25 @@
 const router = require('express').Router();
+const { default: mongoose } = require('mongoose');
 const Medicamento = require('../models/Medicamento');
 
 router.get('/inventario', async (req,res) =>{
     const meds = await Medicamento.find().lean();
     console.log(meds);
     res.render('inventario/inventario', {meds:meds});
-})
+});
+
+router.post('/addMed', async (req,res)=>{
+    const {sku, nombre, comprimidos, gramos} = req.body;
+    //res.send('se agrego un medicamento');
+    const newMedicamento = new Medicamento({
+        _id: new mongoose.Types.ObjectId(),
+        sku,
+        nombre,
+        comprimidos,
+        gramos
+    });
+    await newMedicamento.save();
+    res.redirect('back');
+});
 
 module.exports = router;
