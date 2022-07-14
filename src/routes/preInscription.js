7c17e402627bc32065a835ac27fbc7b2eb3cb918
Esteban Manríquez <esteban.manriquez@sansano.usm.cl>
2022-07-14 13:41:18 -0400

    Preinscription support

--- src/routes/preInscription.js
+++ src/routes/preInscription.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
+const {default:mongoose} = require('mongoose');
+const Prescripcion = require('../models/Prescripcion');
 
-router.get('/preInscription', (req,res) =>{
-    res.render('preInscription/preInscription');
+router.get('/preInscription', async (req,res) =>{
+    const preins = await Prescripcion.find().lean();
+    console.log(preins)
+    res.render('preInscription/preInscription',{preins:preins});
 });
 
 module.exports = router;
