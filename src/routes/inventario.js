11ab6e1b39d26dce948e66ad71031bdcc6a0bb4f
DreWulff <awulff98@gmail.com>
2022-07-14 03:04:45 -0400

    Sweet Dany and Lisa

--- src/routes/inventario.js
+++ src/routes/inventario.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const Medicamento = require('../models/Medicamento');
 
-router.get('/inventario', (req,res) =>{
-    res.render('inventario/inventario', {});
+router.get('/inventario', async (req,res) =>{
+    const meds = await Medicamento.find().lean();
+    console.log(meds);
+    res.render('inventario/inventario', {meds:meds});
 })
 
 module.exports = router;
